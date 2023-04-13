df6afd695eb97ef0a9c79c89c3a19bacd5511495
Camilo Diaz <diazc.pr7966@gmail.com>
2023-04-13 16:24:42 -0500

    empezando con la primera peticion y probando

---
+++ codigo.js
@@ -0,0 +1,29 @@
+
+
+const probar = ()=>{
+    fetch("http://localhost:8080/api/pruebas")
+        .then(res => {
+            console.log(res);
+        }).catch(e => console.log(e))
+}
+const getDatos = ()=>{
+    fetch("http://localhost:8080/api/actividades")
+        .then(res => {
+            if(res.ok){
+                console.log("hola");
+                Promise.resolve(res);
+            }else{
+                Promise.reject(res);
+            }
+        }).catch(e => {
+            console.log(e);
+            console.log("su puta madre hijo de puta");
+            location.href ="https://google.com";
+        })
+}
+
+addEventListener("load", x => {
+    console.log("holaaaa")
+    getDatos();
+    // probar();     
+})
