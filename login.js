5603efcd4b6d68b47ba2f8094e15ff2df1694266
Camilo Diaz <diazc.pr7966@gmail.com>
2023-04-14 17:37:58 -0500

    empezando el codigo js

---
+++ login.js
@@ -0,0 +1,17 @@
+document.querySelector(".login").addEventListener("click", x => {
+    console.log("si me acuerdo");
+    const usuario = document.querySelector(".username").value;
+    const contraseña = document.querySelector(".password").value;
+    let body = `
+        {
+            username : ${usuario},
+            password : ${contraseña}
+        }
+    `;
+    console.log(body);
+
+    document.getElementById("form-login").reset();
+})
+document.querySelector(".create").addEventListener("click", x =>{
+    location.href ="https://google.com";
+})
